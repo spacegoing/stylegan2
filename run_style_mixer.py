601299f68cf8444e96032f4a5d0eea869207da3f
Nikhil Tirumala <nikhil.tiru@gmail.com>
2020-01-31 18:32:46 +0530

    add expression & makeup modes to style mixing + some formatting

--- run_style_mixer.py
+++ run_style_mixer.py
@@ -17,14 +17,14 @@ import pretrained_networks
 
 #----------------------------------------------------------------------------
 
-def style_mixing_example(network_pkl, row_imgs_dir, col_imgs_dir, col_ratio, col_styles, minibatch_size=4):
-    print('Loading networks from "%s"...' % network_pkl)
-    _G, _D, Gs = pretrained_networks.load_networks(network_pkl)
-
-    Gs_syn_kwargs = dnnlib.EasyDict()
-    Gs_syn_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    Gs_syn_kwargs.randomize_noise = False
-    Gs_syn_kwargs.minibatch_size = minibatch_size
+def style_mixing_example(network_pkl, row_imgs_dir, col_imgs_dir, mode, col_ratio, col_styles, minibatch_size=4):
+    print(f'mode is {mode}')
+    if mode == 'expression':
+        col_styles = _parse_num_range('0-7')
+        col_ratio = 0.7
+    if mode == 'makeup':
+        col_styles = _parse_num_range('5-17')
+        col_ratio = 1.0
 
     print("Loading W vectors...")
     row_dlatents_files = glob.glob(row_imgs_dir + '/*.npy')
@@ -46,19 +46,31 @@ def style_mixing_example(network_pkl, row_imgs_dir, col_imgs_dir, col_ratio, col
 
     w_dict = {fl: w for fl, w in zip(all_files, list(all_w))} # [layer, component]
 
+    print('Loading networks from "%s"...' % network_pkl)
+    _G, _D, Gs = pretrained_networks.load_networks(network_pkl)
+
+    Gs_syn_kwargs = dnnlib.EasyDict()
+    Gs_syn_kwargs.output_transform = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+    Gs_syn_kwargs.randomize_noise = False
+    Gs_syn_kwargs.minibatch_size = minibatch_size
+
     print('Generating images...')
     all_images = Gs.components.synthesis.run(all_w, **Gs_syn_kwargs) # [minibatch, height, width, channel]
     image_dict = {(files, files): image for files, image in zip(all_files, list(all_images))}
+    latent_dict = {(files, files): w for files, w in zip(all_files, list(all_w))}
 
+    # col_styles_2 = _parse_num_range('0-6')
     print('Generating style-mixed images...')
-    # Instead of completely replacing the column styles from another image, I felt results are better if you mix them
-    # in a ratio. Here, by default mixing 70% of column image with 30% of row image.
-    # Original idea from https://github.com/iyaja/stylegan-encoder/blob/master/generate_GoT_characters_with_StyleGAN.ipynb
     for row_file in row_dlatents_files:
         for col_file in col_dlatents_files:
             w = w_dict[row_file].copy()
             avg_col_styles = w_dict[col_file][col_styles]*col_ratio + w_dict[row_file][col_styles]*(1-col_ratio)
             w[col_styles] = avg_col_styles
+            # if you want to mix 2 separate sets of column styles.
+            # avg_col_styles = w_dict[col_file][col_styles_2]*(1-col_ratio) + w_dict[row_file][col_styles_2]*(col_ratio)
+            # avg_col_styles = w_dict[col_file][col_styles_2]*(1-col_ratio) + w_dict[row_file][col_styles_2]*(col_ratio)
+            # w[col_styles_2] = avg_col_styles
+            latent_dict[(row_file, col_file)] = w
             image = Gs.components.synthesis.run(w[np.newaxis], **Gs_syn_kwargs)[0]
             image_dict[(row_file, col_file)] = image
 
@@ -66,7 +78,11 @@ def style_mixing_example(network_pkl, row_imgs_dir, col_imgs_dir, col_ratio, col
     for (row_file, col_file), image in image_dict.items():
         r = row_file.split('/')[-1].replace('_01.npy', '')
         c = col_file.split('/')[-1].replace('_01.npy', '')
-        PIL.Image.fromarray(image, 'RGB').save(dnnlib.make_run_dir_path('%s-%s.png' % (r, c)))
+        base_name = dnnlib.make_run_dir_path('%s-%s' % (r, c))
+        img_name = base_name + '.png'
+        npy_name = base_name + '.npy'
+        PIL.Image.fromarray(image, 'RGB').save(img_name)
+        np.save(npy_name, latent_dict[(row_file, col_file)])
 
     print('Saving image grid...')
     _N, _C, H, W = Gs.output_shape
@@ -99,62 +115,71 @@ def _parse_num_range(s):
 
 _examples = '''examples:
 
-  # Generate style mixing example (matches style mixing video clip)
-  python %(prog)s style-mixing-example --network=ffhq_network/stylegan2-ffhq-config-f.pkl
-  --row-imgs-dir=generated_images --col-imgs-dir=generated_images
+  python %(prog)s --network=models/stylegan2-ffhq-config-f.pkl
+  --row-imgs-dir=jdepp --col-imgs-dir=mrbean
+  --mode='expression'
+
+  python %(prog)s --network=models/stylegan2-ffhq-config-f.pkl
+  --row-imgs-dir=jdepp --col-imgs-dir=avatars
+  --mode='makeup'
+
+  ---------- custom modes ------------
+  python %(prog)s --network=models/stylegan2-ffhq-config-f.pkl
+  --row-imgs-dir=avatars --col-imgs-dir=avatars
   --col-styles='0-6' --col-ratio=0.7
+
+  python %(prog)s --network=models/stylegan2-ffhq-config-f.pkl
+  --row-imgs-dir=avatars --col-imgs-dir=jdepp
+  --col-styles='0-6' --col-ratio=0.7
+
+  python %(prog)s --network=models/stylegan2-ffhq-config-f.pkl
+  --row-imgs-dir=mrbean --col-imgs-dir=jdepp
+  --col-styles='0-7' --col-ratio=0.7
+
 '''
 
 #----------------------------------------------------------------------------
 
 def main():
     parser = argparse.ArgumentParser(
-        description='''StyleGAN2 generator.
-
-Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
+        description='StyleGAN2 style mixer - mixes styles of row and column images.',
         epilog=_examples,
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
-    subparsers = parser.add_subparsers(help='Sub-commands', dest='command')
-
-    parser_style_mixing_example = subparsers.add_parser('style-mixing-example', help='Generate style mixing video')
-    parser_style_mixing_example.add_argument('--network',
-                            default='ffhq_network/stylegan2-ffhq-config-f.pkl',
+    parser.add_argument('--network',
+                            default='models/stylegan2-ffhq-config-f.pkl',
                             help='Network pickle filename', dest='network_pkl') #, required=True)
-    parser_style_mixing_example.add_argument('--row-imgs-dir', 
+    parser.add_argument('--row-imgs-dir',
                             help='Files for image rows', required=True)
-    parser_style_mixing_example.add_argument('--col-imgs-dir', 
+    parser.add_argument('--col-imgs-dir',
                             help='Files for image columns', required=True)
-    parser_style_mixing_example.add_argument('--col-styles', 
-                            type=_parse_num_range, 
+    parser.add_argument('--mode', default='custom',
+                            help="'expression' or 'makeup'. If this value is set, col_styles and col_ratio are ignored")
+    parser.add_argument('--col-styles',
+                            type=_parse_num_range,
                             help='Style layer range (default: %(default)s)', default='0-6')
-    parser_style_mixing_example.add_argument('--col-ratio', 
-                            type=float, 
+    parser.add_argument('--col-ratio',
+                            type=float,
                             help='Column Ratio (default: %(default)s)', default=0.7)
-    parser_style_mixing_example.add_argument('--result-dir', 
-                            help='Root directory for run results (default: %(default)s)', 
+    parser.add_argument('--result-dir',
+                            help='Root directory for run results (default: %(default)s)',
                             default='results', metavar='DIR')
 
     args = parser.parse_args()
     kwargs = vars(args)
-    subcmd = kwargs.pop('command')
 
-    if subcmd is None:
-        print ('Error: missing subcommand.  Re-run with --help for usage.')
-        sys.exit(1)
+    row_base_name = args.row_imgs_dir.split('/')[-2]
+    col_base_name = args.col_imgs_dir.split('/')[-2]
 
     sc = dnnlib.SubmitConfig()
     sc.num_gpus = 1
     sc.submit_target = dnnlib.SubmitTarget.LOCAL
     sc.local.do_not_copy_source_files = True
     sc.run_dir_root = kwargs.pop('result_dir')
-    sc.run_desc = subcmd
+    sc.run_desc = args.mode + '_' + row_base_name + '_' + col_base_name
 
-    func_name_map = {
-        'style-mixing-example': 'run_style_mixer.style_mixing_example'
-    }
-    dnnlib.submit_run(sc, func_name_map[subcmd], **kwargs)
+    dnnlib.submit_run(sc, 'run_style_mixer.style_mixing_example', **kwargs)
 
 #----------------------------------------------------------------------------
 
